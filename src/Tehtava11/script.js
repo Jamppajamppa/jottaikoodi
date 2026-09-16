06c7410aa520edc4bf8f6466d276d5eeffec076d
JAMI NOUSIAINEN <TVT_jami.nousiainen@tvtedu.fi>
2026-09-16 19:32:39 +0300

    kesken

---
+++ src/Tehtava11/script.js
@@ -0,0 +1,53 @@
+class Product {
+
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    printDetails() {
+        return `<tr><td>${this.name}</td><td>${this.price}</td></tr>`;
+    }
+}
+updateProductList();
+
+function updateProductList() {
+const productList = [];
+    fetch("api.php", {
+        method: "GET"
+    })
+    .then(response => response.json())
+    .then(data => {
+        data.foreach(product => {
+            productList.push(new Product(product.name, product.price));
+
+            let products = "";
+            for (let i = 0; i < productList.length; i++) {
+                products += productList[i].printDetails();
+            }
+            document.getElementById("productList").innerHTML = products;
+        })
+    });
+}
+
+function addProduct() {
+    const name = document.getElementById("name").value;
+    const price = document.getElementById("price").value;
+
+    const product = new Product(name, price);
+
+fetch("api.php", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(product)
+})
+
+.then(response => response.json())
+
+.then(data => {
+    document.getElementById("msg").textContent = data.success ? "Tuote lisätty onnistuneesti!" : "Tuotteen lisääminen epäonnistui.";
+    updateProductList();
+});
+}
